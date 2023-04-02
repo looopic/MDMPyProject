8beb979815ecb179cf7a237d3c6c7d18a323b84c
Looopic <carlo.huser@gmail.com>
2023-04-02 19:36:45 +0200

    Changed \ to /

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__, static_url_path='/', static_folder='web')
 
 @app.route("/")
 def indexPage():
-    return send_file("web\index.html")
+    return send_file("web/index.html")
 
 
 @app.route('/submit', methods=['GET', 'POST'])
@@ -27,6 +27,9 @@ def login():
 def getImage(text):
     model_id = "runwayml/stable-diffusion-v1-5"
     pipe = StableDiffusionPipeline.from_pretrained(model_id)
+
+    #with a graphics card, use the following codes
+
     #pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16)
     #pipe = pipe.to("cuda")
     prompt = text
